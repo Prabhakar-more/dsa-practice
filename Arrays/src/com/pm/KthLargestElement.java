ec3d15c09665db7e076af124cf5176451ed6e571
Prabhakar-more <prabhakarmore0403@gmail.com>
2025-11-13 23:05:25 +0530

    initial commit

---
+++ Arrays/src/com/pm/KthLargestElement.java
@@ -0,0 +1,20 @@
+package com.pm;
+
+public class KthLargestElement {
+
+	public static void main(String[] args) {
+		int[] arr = {7, 4, 6, 3, 9, 1, 2, 10, 10};
+
+	}
+
+}
+
+
+/*Problem 4 — “Find the Kth Largest Element (without sorting)”
+
+Description: Given an integer array and an integer k, find the kth largest distinct element in the array. If there are fewer than k distinct elements, print a clear message.
+
+Example:
+Input: arr = {7, 4, 6, 3, 9, 1, 2, 10, 10}, k = 3
+Distinct sorted descending → {10, 9, 7, 6, 4, 3, 2, 1}
+Output: 3rd largest distinct = 7*/
