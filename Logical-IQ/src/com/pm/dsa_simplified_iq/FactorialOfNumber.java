291159bd77c1c7334e99f403f5ebaf46c1c77377
Prabhakar-more <prabhakarmore0403@gmail.com>
2025-11-18 18:16:43 +0530

    initial commit

---
+++ Logical-IQ/src/com/pm/dsa_simplified_iq/FactorialOfNumber.java
@@ -0,0 +1,26 @@
+package com.pm.dsa_simplified_iq;
+
+import java.util.Scanner;
+
+//Factorial of a Number       3!= 3*2*1=6
+public class FactorialOfNumber {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number : ");
+		int number = sc.nextInt();
+		
+		int factorial = 1;
+		
+		for(int i=1; i<=number; i++)
+		{
+			factorial = factorial*i;
+		}
+		
+		System.out.println("Factorial of number "+number+" is : "+factorial);
+		sc.close();
+
+	}
+
+}
